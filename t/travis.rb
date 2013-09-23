ee5b8287e96a4adbed62a1f17195b109d309ba9b
rhysd <lin90162@yahoo.co.jp>
2013-09-23 13:33:32 +0900

    improve a spec name

--- t/travis.rb
+++ t/travis.rb
@@ -4,6 +4,6 @@
 require './t/vspec_helper'
 
 v = Vspec.new(vspec_root: "./vim-vspec")
-v.run "t/vspec.vim", autoloads: ['./vim-vspec-matchers', './vim-textobj-user']
+v.run "t/textobj_anyblock_spec.vim", autoloads: ['./vim-vspec-matchers', './vim-textobj-user']
 puts v.result
 exit 1 if ! v.success? || v.count_failed > 0
